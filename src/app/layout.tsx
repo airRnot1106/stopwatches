2108b4fe02733a83bee9a06acf51331e6a570333
airRnot1106 <yngw.rintaro@gmail.com>
2024-08-11 14:36:11 +0900

    chore: rename molecues to molecules in SiteHeader import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SiteHeader } from "./components/site/molecues/SiteHeader";
+import { SiteHeader } from "./components/site/molecules/SiteHeader";
 
 const inter = Inter({ subsets: ["latin"] });
 
