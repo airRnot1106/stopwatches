3a2f84c9bdc21ad35e23036d3b174cfd69d21252
airRnot1106 <yngw.rintaro@gmail.com>
2024-08-11 16:29:35 +0900

    feat: add ResetAllContext and ResetProvider component

---
+++ src/app/stopwatchs/_providers/reset.tsx
@@ -0,0 +1,34 @@
+import { createContext, type ReactNode, useRef } from "react";
+
+type ResetAllContextProps = {
+  addResetFunction: (resetFn: () => void) => void;
+  resetAll: () => void;
+};
+
+export const ResetAllContext = createContext<ResetAllContextProps | undefined>(
+  undefined,
+);
+
+type ResetProviderProps = {
+  children: ReactNode;
+};
+
+export const ResetProvider = ({ children }: ResetProviderProps) => {
+  const resetFunctions = useRef<(() => void)[]>([]);
+
+  const addResetFunction = (resetFn: () => void) => {
+    resetFunctions.current.push(resetFn);
+  };
+
+  const resetAll = () => {
+    for (const resetFn of resetFunctions.current) {
+      resetFn();
+    }
+  };
+
+  return (
+    <ResetAllContext.Provider value={{ addResetFunction, resetAll }}>
+      {children}
+    </ResetAllContext.Provider>
+  );
+};
